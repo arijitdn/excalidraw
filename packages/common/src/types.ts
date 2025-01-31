e35afaeb331fc4265f44ae6aa6e55e1d8388564d
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-01-31 14:46:42 +0530

    Added @repo/db

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
+  name: z.string().max(20),
   username: z.string().min(3).max(10),
   password: z.string().min(6).max(15),
-  name: z.string().max(20),
 });
 
 export const SignInSchema = z.object({
